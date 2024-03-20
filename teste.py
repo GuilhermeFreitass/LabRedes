c43d9424692d918597a5548d4ab1700ebf3a390a
Guilherme Freitas <117193964+GuilhermeFreitass@users.noreply.github.com>
2024-03-19 22:31:23 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,5 +1,5 @@
-#!/usr/bin/python
-#-*-coding: utf-8-*-
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import socket
 import os
@@ -11,50 +11,52 @@ def get_dns_service():
 
 def get_web_service():
     # Conecta-se ao serviço web para determinar o endereço IP e a porta
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        try:
-            s.connect(('exemplo.psi.br', 80))
-            web_ip, web_port = s.getpeername()
-            return web_ip, web_port
-        except ConnectionRefusedError:
-            return None, None
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        s.connect(('exemplo.psi.br', 80))
+        web_ip, web_port = s.getpeername()
+        return web_ip, web_port
+    except socket.error:
+        return None, None
+    finally:
+        s.close()
 
 def capture_traffic():
     # Captura o tráfego gerado ao testar se os serviços estão funcionando corretamente
     filename = "traffic_capture.pcap"
-    os.system(f"sudo tcpdump -i any -w {filename} host exemplo.psi.br")
-    print("Captura de tráfego concluída.")
+    os.system("sudo tcpdump -i any -w {0} host exemplo.psi.br".format(filename))
+    print "Captura de tráfego concluída."
 
 def test_host_online():
     # Testa se o host está online
     response = os.system("ping -c 1 exemplo.psi.br > /dev/null")
     if response == 0:
-        print("O host está online.")
+        print "O host está online."
     else:
-        print("O host está offline.")
+        print "O host está offline."
 
 def test_web_service(ip, port):
     # Testa se o serviço web está respondendo corretamente
-    response = os.system(f"curl -s {ip}:{port} > /dev/null")
+    response = os.system("curl -s {0}:{1} > /dev/null".format(ip, port))
     if response == 0:
-        print("O serviço web está respondendo corretamente.")
+        print "O serviço web está respondendo corretamente."
     else:
-        print("O serviço web não está respondendo corretamente.")
+        print "O serviço web não está respondendo corretamente."
 
 def main():
     # Obter IPs e portas dos serviços de nome e web
     dns_ip = get_dns_service()
     web_ip, web_port = get_web_service()
 
-    print("Serviço de Nome:")
-    print("IP:", dns_ip)
+    print "Serviço de Nome:"
+    print "IP:", dns_ip
 
     if web_ip:
-        print("\nServiço Web:")
-        print("IP:", web_ip)
-        print("Porta:", web_port)
+        print "\nServiço Web:"
+        print "IP:", web_ip
+        print "Porta:", web_port
     else:
-        print("\nServiço Web indisponível.")
+        print "\nServiço Web indisponível."
 
     # Capturar tráfego
     capture_traffic()
